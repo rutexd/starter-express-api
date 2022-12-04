6b7896ad4cc7793bab414628ca324c245ec44b31
Rutex <rutexgreat@gmail.com>
2022-12-04 19:03:12 +0100

    Webserver

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const ws = require('ws');
+
+const app = express();
+
+// Set up a headless websocket server that prints any
+// events that come in.
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+    socket.on('message', message => socket.send(message));
+});
+
+// `server` is a vanilla Node.js HTTP server, so use
+// the same ws upgrade process described here:
+// https://www.npmjs.com/package/ws#multiple-servers-sharing-a-single-https-server
+const server = app.listen(process.env.PORT || 3000);
+server.on('upgrade', (request, socket, head) => {
+    wsServer.handleUpgrade(request, socket, head, socket => {
+        wsServer.emit('connection', socket, request);
+    });
+});
+
+app.get('/', (req, res) => {
+    res.status(200).send('Hello, world!');
+});
